b06d4d5091c12a4f741acf9a1c857bed62e91d1e
Valentina <valentina.lazaro@studentambassadors.com>
2023-06-14 23:38:28 -0400

    Initial commit

---
+++ app/src/main/java/com/example/proyectomovil/EstadoMateriaActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectomovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EstadoMateriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estado_materia);
+    }
+}
